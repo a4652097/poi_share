8bcabafbaaa5b4f35e63f7e5348c4ab1c2a371c3
leon <ylf09150033@163.com>
2015-03-09 10:50:41 +0800

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'resources#index'
-  resources :resources, only:[:index,:new, :create]
+  resources :resources, only:[:index,:new, :create, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
